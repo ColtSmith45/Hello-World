616713a0acbf539f1c8d8c64661b696f3eff8696
Colt Smith <111935775+ColtSmith45@users.noreply.github.com>
2022-09-04 14:16:57 -0500

    Initial Upload
    Upload of entire project

---
+++ Assets/myFirstScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myFirstScript : MonoBehaviour
+{
+    //
+    void Awake()
+    {
+        Debug.Log("awake"); 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("update");
+    }
+}
